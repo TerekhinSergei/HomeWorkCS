a367a0fb97d193f93e02b4ab6ce18fc3fb2a5bfb
Terekhin <blackrat72@qip.ru>
2022-09-15 22:50:03 +0300

    add Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.Write("введите первое число: ");
+string input1 = Console.ReadLine()!;
+int number1 = int.Parse(input1);
+Console.Write("введите второе число: ");
+string input2 = Console.ReadLine()!;
+int number2 = int.Parse(input2);
+
+if (number1 > number2)
+{
+    Console.WriteLine($"max = {number1}, min = {number2}");
+}
+else
+{
+    Console.WriteLine($"max = {number2}, min = {number1}");
+}
