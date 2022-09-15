209067b4a06d8c33c0d0d5721a160b9152ff7eff
Terekhin <blackrat72@qip.ru>
2022-09-15 23:01:38 +0300

    add Task006

---
+++ Task006/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.WriteLine("введите число: ");
+string a = Console.ReadLine()!;
+int number = int.Parse(a);
+
+int result = number % 2;
+
+if (result == 0 )
+{ 
+Console.WriteLine("ДА - число четное");    
+}
+else
+{
+Console.WriteLine("НЕТ - число нечетное");
+}
