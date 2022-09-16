68d9da5cc3e1d8e548d1ade933cd5f18a772549b
Terekhin <blackrat72@qip.ru>
2022-09-16 18:49:14 +0300

    add Task008

---
+++ Task008/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+Console.WriteLine("введите положительное число больше 1: ");
+string a = Console.ReadLine()!;
+int number = int.Parse(a);
+if (number <= 1)
+{
+    Console.WriteLine("введите положительное число больше 1 !");
+}
+// первый вариант решения (закомментирован)
+/*int count = 2;
+while (count <= number)
+{
+    Console.Write($"{count}, ");
+    count += 2;
+}
+*/
+// второй вариант решения
+int count = 1;
+while (count <= number)
+{
+    int result = count % 2;
+    if (result == 0)
+    {
+        Console.Write($"{count}, ");
+    }
+    count++;
+}
