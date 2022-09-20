a5b8a96c2f923e75f54da94c442cf8d388530693
Terekhin <blackrat72@qip.ru>
2022-09-20 12:47:32 +0300

    add Task013

---
+++ Task013/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int number = (int)Math.Abs(int.Parse(Console.ReadLine()!)); // Берем модуль введеного числа, для упрощения решения 
+
+if (number < 100)
+    Console.WriteLine("У числа нет третьей цифры");
+else
+{
+    while (number > 1000)
+    {
+        number = number / 10;
+    }
+    Console.Write($"третья цифра введенного числа -> {number % 10}");
+}
