6539056f2c261a70699983bb8d5f3d2854d253de
Terekhin <blackrat72@qip.ru>
2022-09-23 22:23:03 +0300

    add Task021

---
+++ Task021/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите координату X для А: ");
+double xa = double.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Y для А: ");
+double ya = double.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Z для А: ");
+double za = double.Parse(Console.ReadLine()!);
+
+Console.Write("Введите координату X для B: ");
+double xb = double.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Y для B: ");
+double yb = double.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Z для B: ");
+double zb = double.Parse(Console.ReadLine()!);
+
+
+double lgth = Math.Sqrt(Math.Pow(xa - xb, 2) + Math.Pow(ya - yb, 2) + Math.Pow(za - zb, 2));
+//d = Math.Round(d, 2);
+Console.Write($"длина отрезка А({xa},{ya},{za}) B({xb},{yb},{zb}) = {lgth:f2}");
