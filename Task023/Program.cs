945318f0f5286d668db7238b6d89439ab92325ee
Terekhin <blackrat72@qip.ru>
2022-09-23 22:23:41 +0300

    add Task023

---
+++ Task023/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write("Введите число N: ");
+int number = int.Parse(Console.ReadLine()!);
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write(Math.Pow(i, 3));
+    if (i != number)
+        Console.Write(", ");
+    else
+        Console.WriteLine(".");
+}
