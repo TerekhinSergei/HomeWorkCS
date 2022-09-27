6d8b70fde83c6d54662477494bc9384413cab92f
Terekhin <blackrat72@qip.ru>
2022-09-27 11:54:55 +0300

    add Task029

---
+++ Task029/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая задает массив из 8 элементов и выводит их на экран
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+// Ограничения:
+// - Должна быть отдельная функция, которая создаёт массив и отдельная функция вывода
+// - Внутри функций (кроме функции вывода) НЕЛЬЗЯ использовать класс Console. Только в основном блоке кода или в функции вывода
+
+Console.Clear();
+
+int[] nums = new int[8];
+FillArray(nums);
+PrintArray(nums);
+
+void FillArray(int[] array)
+{
+    Random rand = new Random();    
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next();
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(array[i]);
+        if (i == count - 1)            
+            Console.WriteLine("]");
+        else
+            Console.Write(", ");
+    }    
+}
