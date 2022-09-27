205f9eecfeb1329cd750b0addfc8a5e02059bc51
Terekhin <blackrat72@qip.ru>
2022-09-27 11:43:51 +0300

    add Task027

---
+++ Task027/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+// Ограничения:
+// - Должна быть отдельная функция, которая возвращает результат суммы цифр
+// - Внутри функций НЕЛЬЗЯ использовать класс Console. Только в основном блоке кода
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = (int)Math.Abs(int.Parse(Console.ReadLine()!)); // так как нам нужны только цифры берем модуль веденного числа
+Console.WriteLine($"сумма цифр в введенном числе = {NumSumma(number)}");
+
+int NumSumma(int number)
+{
+    string num = number.ToString();
+    int res = 0;
+    for (int i = 0; i < num.Length; i++)
+    {
+        res = res + number % 10;
+        number = number / 10;        
+    }
+    return res;
+}
+
+
