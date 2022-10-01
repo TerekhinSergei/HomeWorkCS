0843ec5f87e9ba31010241ca1d9b03a9d7e4780b
Terekhin <blackrat72@qip.ru>
2022-10-01 20:00:54 +0300

    add Task034

---
+++ Task034/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+//////////
+Console.Clear();
+
+Console.Write("Введите размерность массива: ");
+int length = int.Parse(Console.ReadLine()!);
+int[] nums = CreateArray(length);
+PrintArray(nums);
+Console.WriteLine($"количество четных элементов в массиве = {CountEvenElements(nums)}");
+
+int[] CreateArray(int length)
+{
+    nums = new int[length];
+    Random rand = new Random();    
+    for (int i = 0; i < nums.Length; i++)
+    {
+        nums[i] = rand.Next(100, 1000);
+    }
+    return nums;
+}
+int CountEvenElements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (array[i]%2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(array[i]);
+        if (i == count - 1)            
+            Console.WriteLine("]");
+        else
+            Console.Write(", ");
+    }    
+}
