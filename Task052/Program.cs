78ce14c456881f973bfc09872c1d4ee1a67c7430
Terekhin <blackrat72@qip.ru>
2022-10-07 21:08:44 +0300

    add Task052

---
+++ Task052/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4   Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+
+Console.Write("Задайте размерность массива m x n.\nВведите m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine($"Среднее арифметическое столбцов -> {String.Join("; ", getAverageСolumn(array))}");
+
+
+double[] getAverageСolumn(int[,] array)
+{
+    double[] averageСolumn = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum = sum + array[i, j];
+        }
+        averageСolumn[j] = Math.Round(sum / array.GetLength(0), 2);
+    }
+    return averageСolumn;
+}
+
+void FillArray(int[,] array)
+{
+    Random generator = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = generator.Next(-9, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < 0) // для красоты представления массива
+                Console.Write($"{array[i, j]}  ");
+            else
+                Console.Write($" {array[i, j]}  ");
+        }
+        Console.WriteLine("");
+    }
+}
+
